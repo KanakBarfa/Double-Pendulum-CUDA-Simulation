387d65c074a63ceb6a87f9eb49f8de8698bb9e23
kanak <kanakbarfa@gmail.com>
2026-05-28 19:17:11 +0530

    Added RK4

--- main.cpp
+++ main.cpp
@@ -1,73 +1,85 @@
 #include <iostream>
 #include <cmath>
-#include <deque>
-#include <SFML/Graphics.hpp>
-using namespace std;
+#include <numbers>
+#include <array>
+#define M_PII 3.14159265358979323846f
+#define MAX_ITER 5000000
+const float g = 9.81f;
+const float mass = 1.0f;
+const float timeStep = 0.01f;
+const float l = 1.0f;
 
-const float g = 9.81;        // Acceleration due to gravity
-const float mass = 1.0;      // Mass of the object
-const float timeStep = 0.01; // Time step for simulation
-const float l = 1.0;         // Length of the pendulum
-const int resolution = 1000;     // Number of points in the graph
+inline std::array<float, 4> calc_derivatives(const std::array<float, 4>& state) {
+    float t1 = state[0];
+    float t2 = state[1];
+    float w1 = state[2];
+    float w2 = state[3];
 
-inline void calc_position(float &theta1, float &theta2, float &omega1,
-                          float &omega2)
-{
-    float alpha1 = (-g * (3.0f * sin(theta1) + sin(theta1 - 2.0f * theta2)) -
-                    2.0f * sin(theta1 - theta2) * (omega2 * omega2 * l + omega1 * omega1 * l * cos(theta1 - theta2))) /
-                   (l * (3.0f - cos(2.0f * theta1 - 2.0f * theta2)));
+    float delta = t1 - t2;
+    float denom = l * (3.0f - std::cos(2.0f * t1 - 2.0f * t2));
 
-    float alpha2 = (2.0f * sin(theta1 - theta2) * (2.0f * omega1 * omega1 * l + 2.0f * g * cos(theta1) + omega2 * omega2 * l * cos(theta1 - theta2))) /
-                   (l * (3.0f - cos(2.0f * theta1 - 2.0f * theta2)));
+    float a1 = (-g * (3.0f * std::sin(t1) + std::sin(t1 - 2.0f * t2)) -
+                2.0f * std::sin(delta) * (w2 * w2 * l + w1 * w1 * l * std::cos(delta))) / denom;
 
-    omega1 += alpha1 * timeStep;
-    omega2 += alpha2 * timeStep;
+    float a2 = (2.0f * std::sin(delta) * (2.0f * w1 * w1 * l + 2.0f * g * std::cos(t1) +
+                w2 * w2 * l * std::cos(delta))) / denom;
 
-    theta1 += omega1 * timeStep;
-    theta2 += omega2 * timeStep;
+    return {w1, w2, a1, a2};
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    float theta1 = 1.0;  // Initial angle in degrees
-    float theta2 = 5.0;  // Initial angle in degrees
-    float omega1 = 0.0;   // Initial angular velocity
-    float omega2 = 0.0;   // Initial angular velocity
+void rk4_step(std::array<float, 4>& state) {
+    std::array<float, 4> k1 = calc_derivatives(state);
 
-    // cout << "Enter the angle in degrees: ";
-    cin >> theta1 >> theta2;
-    theta1 = theta1 * M_PI / 180.0; // Convert to radians
-    theta2 = theta2 * M_PI / 180.0; // Convert to radians
+    std::array<float, 4> state_k2;
+    for (int i = 0; i < 4; ++i) state_k2[i] = state[i] + 0.5f * timeStep * k1[i];
+    std::array<float, 4> k2 = calc_derivatives(state_k2);
 
-    float ptheta1, ptheta2;
+    std::array<float, 4> state_k3;
+    for (int i = 0; i < 4; ++i) state_k3[i] = state[i] + 0.5f * timeStep * k2[i];
+    std::array<float, 4> k3 = calc_derivatives(state_k3);
+
+    std::array<float, 4> state_k4;
+    for (int i = 0; i < 4; ++i) state_k4[i] = state[i] + timeStep * k3[i];
+    std::array<float, 4> k4 = calc_derivatives(state_k4);
+
+    for (int i = 0; i < 4; ++i) {
+        state[i] += (timeStep / 6.0f) * (k1[i] + 2.0f * k2[i] + 2.0f * k3[i] + k4[i]);
+    }
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    float theta1, theta2;
+    if (!(std::cin >> theta1 >> theta2)) return 0;
+
+    std::array<float, 4> state = {
+        theta1 * M_PII / 180.0f,
+        theta2 * M_PII / 180.0f,
+        0.0f,
+        0.0f
+    };
 
     int counter = 0;
 
-    while (true)
-    {
-
-        ptheta1 = theta1;
-        ptheta2 = theta2;
-
-        calc_position(theta1, theta2, omega1, omega2);
-
+    while (true) {
+        rk4_step(state);
         counter++;
 
-        // cout<<counter<<" "<<theta1<<" "<<theta2<<'\n';
-        if (theta1 < - M_PI || theta2 < - M_PI)
-        {
+        if (state[0] < -M_PII || state[1] < -M_PII ||
+            state[0] > M_PII || state[1] > M_PII) {
             break;
         }
 
-        if (theta1 > M_PI || theta2 > M_PI)
-        {
+        if (counter > MAX_ITER) {
+            std::cout << "Simulation stabilized without flipping.\n";
             break;
         }
-
     }
-    cout << "Final angles (degrees): " << theta1 * 180.0 / M_PI << " " << theta2 * 180.0 / M_PI << '\n';
-    cout << "Number of iterations: " << counter << '\n';
+
+    std::cout << "Final angles (degrees): " << state[0] * 180.0f / M_PII << " "
+              << state[1] * 180.0f / M_PII << '\n';
+    std::cout << "Number of iterations: " << counter << '\n';
     return 0;
 }
